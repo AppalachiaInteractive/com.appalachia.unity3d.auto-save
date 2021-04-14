3a62305c3f31642c2d3447ed101c856fe24fc069
Chris Schubert <chris.a.schubert25@gmail.com>
2021-04-14 19:36:30 -0400

    Cleanup of temp files from publish

--- .package/preversion.sh
+++ .package/preversion.sh
@@ -1,5 +1,7 @@
 version=$npm_package_version
 
+git fetch -p
+
 tag=$(git describe --abbrev=0)
 previous_tag=$(git describe --abbrev=0 $tag^)
 previous_tag_hash=$(git rev-list -n 1 $previous_tag)
@@ -10,13 +12,12 @@ clean_message=$(echo "$message" | sed -z 's/\n/\\n/g')
 find="## Unreleased"
 replace="## Unreleased\n\n## $version\n$clean_message"
 
-echo '--------------'
-#cat CHANGELOG.md
 mv CHANGELOG.md CHANGELOG.md.bk
+
 sed "s|$find|$replace|g" CHANGELOG.md.bk > CHANGELOG.md.bk2
-echo '--------------'
-#cat CHANGELOG.md
 sed "s|\n\n\n|\n\n|g" CHANGELOG.md.bk2 > CHANGELOG.md
-echo '--------------'
-cat CHANGELOG.md
-echo '--------------'
+
+rm CHANGELOG.md.bk
+rm CHANGELOG.md.bk2
+
+git add .
