155c87dbf2dbb066bc89c18be2c2d3753b7180dc
Chris Schubert <chris.a.schubert25@gmail.com>
2021-04-14 18:34:41 -0400

    Fixing error code checking in npm publich shell script

--- publish.sh
+++ publish.sh
@@ -2,7 +2,11 @@
 
 if [[ "$1" == "patch" || "$1" == "minor" || "$1" == "major" || "$1" == "prepatch" || "$1" == "preminor" || "$1" == "premajor" || "$1" == "prerelease" ]]; then
     npm version $1
-    npm publish --registry "http://localhost:4873"
+    
+    if [ $? -eq 0 ]; then
+        npm publish --registry "http://localhost:4873"
+        exit 1
+    fi
 
 else
     echo "Choose [patch, minor, major, prepatch, preminor, premajor, prerelease]"
