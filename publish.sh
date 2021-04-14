b3991c78ae91e4c78ba616b9c58483d9899442eb
Chris Schubert <chris.a.schubert25@gmail.com>
2021-04-14 19:14:12 -0400

    Adding github release to publish script

--- publish.sh
+++ publish.sh
@@ -3,10 +3,61 @@
 if [[ "$1" == "patch" || "$1" == "minor" || "$1" == "major" || "$1" == "prepatch" || "$1" == "preminor" || "$1" == "premajor" || "$1" == "prerelease" ]]; then
     npm version $1
     
-    if [ $? -eq 0 ]; then
-        npm publish --registry "http://localhost:4873"
-        exit 1
+    if [ $? -ne 0 ]; then
+        exit $?        
     fi
+
+    package_version=$(cat package.json \
+    | grep version \
+    | head -1 \
+    | awk -F: '{ print $2 }' \
+    | sed 's/[",]//g'
+    | tr -d '[[:space:]]')
+
+    rm -f .dist
+    
+    if [ $? -ne 0 ]; then
+        exit $?        
+    fi
+
+    mkdir .dist
+    
+    if [ $? -ne 0 ]; then
+        exit $?        
+    fi
+
+    cd .dist
+
+    npm pack ..
+
+    if [ $? -ne 0 ]; then
+        exit $?        
+    fi
+
+    cd ..
+
+    package=`ls .dist | head -n 1`
+
+    echo "Publishing"
+
+    npm publish $package --registry "http://localhost:4873"
+    
+    if [ $? -ne 0 ]; then
+        exit $?        
+    fi 
+    
+    #use release notes from a file
+    echo "Sending to github as release"
+
+    gh release create $package_version .dist/*.tgz -F CHANGELOG.md
+    if [ $? -ne 0 ]; then
+        exit $?        
+    fi 
+
+    echo "Destroying tarballs"
+    
+    rm -f .dist
+
 
 else
     echo "Choose [patch, minor, major, prepatch, preminor, premajor, prerelease]"
